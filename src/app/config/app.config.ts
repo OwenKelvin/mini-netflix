b566a2d8300d5f7a2db0d479734baf96a98ee15c
Owen Kelvin <otienoowenkelvin@gmail.com>
2019-10-21 13:08:40 +0300

    Changed Backend URL to heroku

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,2 +1,3 @@
-export const backendUrl = 'http://localhost:3000/';
+// export const backendUrl = 'http://localhost:3000/';
+export const backendUrl = 'https://owen-kelvin-mini-netflix.herokuapp.com/';
 export const imageUrl = 'https://image.tmdb.org/t/p/original';
